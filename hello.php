3fe331483a02954ea2147e493adf8b6ca51be22f
martpors <M2rtpors@hotmail.com>
2024-09-04 14:11:50 +0300

    ja

---
+++ hello.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$a = 3;
+$b = 5;
+
+if ( $a > $b ) {
+    echo $a;
+} else {
+    echo $b;
+}
+
+echo "\n\n";
+
+//  var_dump($_GET);
+
+
+
+// $user = "Märt";
+
+// $a = 5;
+// $b = 3;
+
+// echo $user . 3;
+
+// echo "<br><br>";
+
+// printf("%u + %u = %u", $a, $b, $a + $b);
+
+// echo "<br><br>";
+
+// printf("$a + $b = $a . $b");
+
+// echo "<br><br>";
+
+// echo "Hello, $user!";
+
+// echo "<br><br>";
+// echo 'Tere, $user!';
+
+// echo "<br><br>";
+// echo 'Hello, ' .$user . '!';
+
+// echo "<br><br>";
+
+// printf("Hello,%s!", $user);
+
+
+?>
+
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,9 @@
+apache2
+nginx
+
+pordid
+80      http
+413     https
+22      Ssh
+3306    mysql
+21      ftp
